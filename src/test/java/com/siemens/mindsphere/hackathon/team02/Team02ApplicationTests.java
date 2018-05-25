64f8425f02361f387d03bfa6d368ef9380d78f62
Thanooj Kamavarapu <thanooj.kamavarapu@cloudtp.com>
2018-05-25 11:03:40 -0400

    Intitial Spring Boot commit

---
+++ src/test/java/com/siemens/mindsphere/hackathon/team02/Team02ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.siemens.mindsphere.hackathon.team02;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Team02ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
